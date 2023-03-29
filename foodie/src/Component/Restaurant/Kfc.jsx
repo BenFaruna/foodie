90b50db4251310b894525499124826667eb6b30c
bhankymyk <bankole123michael@gmail.com>
2023-03-29 01:31:15 +0100

    bulgaria in process

---
+++ foodie/src/Component/Restaurant/Kfc.jsx
@@ -0,0 +1,25 @@
+            {
+                "_id": "642150bfdefea0673f3e38e4",
+                "name": "Extra Large Fries",
+                "description": "Fried Potatoes perfectly paired with Our secret hot sauce"
+            },
+            {
+                "_id": "642150c5defea0673f3e38e8",
+                "name": "Zinger Triple Threat Friday",
+                "description": "hree Delicious Burgers with 40 percent Discount every Friday"
+            },
+            {
+                "_id": "642150cadefea0673f3e38ec",
+                "name": "MVP Box",
+                "description": "Enjoy 3 pieces of deep fried Chicken, 1 Regular Chips and 500ml Pepsi"
+            },
+            {
+                "_id": "642150d1defea0673f3e38f0",
+                "name": "Double Zinger Burger Meal",
+                "description": "1 Double Zinger Burger, Regular chips,  500ml Pepsi"
+            },
+            {
+                "_id": "642150d7defea0673f3e38f4",
+                "name": "21 Pieces KFC Bucket",
+                "description": "T21 Pieces of Hot and Crispy Original Chicken with 1.5L Pepsi"
+            }
