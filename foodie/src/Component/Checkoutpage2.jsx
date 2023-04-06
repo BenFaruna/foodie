a0cdd69e20aeae2cc472202665c85d597365d3cb
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-04-06 12:45:24 +0100

    checkou page done

--- foodie/src/Component/Checkoutpage2.jsx
+++ foodie/src/Component/Checkoutpage2.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from '../Styles/checkoutpage2.css';
+import styles from '../Styles/checkoutpage2.module.css';
 import {Link} from 'react-router-dom';
 
 class Checkoutpage2 extends Component {
@@ -31,79 +31,79 @@ class Checkoutpage2 extends Component {
     <div>
       <h2>Checkout</h2>
       {/* codes for the phoneNumber block */}
-      <div className='phn-no'>
-        <div className="phn-no-1">
+      <div className={`${styles.phn_no}`}>
+        <div className={`${styles.phn_no_1}`}>
             <span>Phone Number</span>
             {/* the phoneNumber property is called here */}
-            <div className="phn-no-2">{this.state.phoneNumber}</div>
+            <div className={`${styles.phn_no_2}`}>{this.state.phoneNumber}</div>
         </div>
-        <div className="phn-no-ed">Edit</div>
+        <div className={`${styles.phn_no_ed}`}>Edit</div>
       </div>
       {/* codes for the Payment details block*/}
-      <div className='pay-det'>
-        <div className="pay-det-1">
+      <div className={`${styles.pay_det}`}>
+        <div className={`${styles.pay_det_1}`}>
             <span>Payment Details</span>
             {/* The different payment option properties are called here */}
-            <div className="pay-det-2">{this.state.payment1}, {this.state.payment2}, and {this.state.payment3}</div>
+            <div className={`${styles.pay_det_2}`}>{this.state.payment1}, {this.state.payment2}, and {this.state.payment3}</div>
         </div>
-        <div className="pay-det-ed">Edit</div>
+        <div className={`${styles.pay_det_ed}`}>Edit</div>
       </div>
       {/* codes for order details block*/}
-      <div className='ord-det'>
+      <div className={`${styles.ord_det}`}>
         <span>Your Order Details</span>
         {/* The different order options tab */}
-        <div className="ord-det-tab">
-          <div className="ord-det-tab-1">
+        <div className={`${styles.ord_det_tab}`}>
+          <div className={`${styles.ord_det_tab_1}`}>
             {/* the quantity tab */}
-            <div className="ord-det-tab-qty">
-              <article className="ord-det-tab-qty-1">{this.state.orderQty}</article>
-              <article className="ord-det-tab-qty-2">{this.state.orderQty}</article>
+            <div className={`${styles.ord_det_tab_qty}`}>
+              <article className={`${styles.ord_det_tab_qty_1}`}>{this.state.orderQty}</article>
+              <article className={`${styles.ord_det_tab_qty_2}`}>{this.state.orderQty}</article>
             </div>
             {/* the order description tab */}
-            <div className="ord-det-tab-des">
-              <article className="ord-det-tab-des-1">{this.state.orderDes1}</article>
-              <article className="ord-det-tab-des-2">{this.state.orderDes2}</article>
+            <div className={`${styles.ord_det_tab_des}`}>
+              <article className={`${styles.ord_det_tab_des_1}`}>{this.state.orderDes1}</article>
+              <article className={`${styles.ord_det_tab_des_2}`}>{this.state.orderDes2}</article>
             </div>
             {/* the bills tab */}
-            <div className="ord-det-tab-bil">
-              <article className="ord-det-tab-bil-1">{this.state.bill1}</article>
-              <article className="ord-det-tab-bil-2">{this.state.bill2}</article>
+            <div className={`${styles.ord_det_tab_bil}`}>
+              <article className={`${styles.ord_det_tab_bil_1}`}>{this.state.bill1}</article>
+              <article className={`${styles.ord_det_tab_bil_2}`}>{this.state.bill2}</article>
             </div>
           </div>
           {/* subtotal */}
-          <div className="ord-det-tab-2">
-            <article className="ord-det-tab-sub">SubTotal</article>
-            <article className="ord-det-tab-sub-2">{this.state.subTot}</article>
+          <div className={`${styles.ord_det_tab_2}`}>
+            <article className={`${styles.ord_det_tab_sub}`}>SubTotal</article>
+            <article className={`${styles.ord_det_tab_ub_2}`}>{this.state.subTot}</article>
           </div>
           {/* vat */}
-          <div className="ord-det-tab-3">
-            <article className="ord-det-tab-vat">Vat (7.50%)</article>
-            <article className="ord-det-tab-vat-3">{this.state.vat}</article>
+          <div className={`${styles.ord_det_tab_3}`}>
+            <article className={`${styles.ord_det_tab_vat}`}>Vat (7.50%)</article>
+            <article className={`${styles.ord_det_tab_vat_3}`}>{this.state.vat}</article>
           </div>
           {/* delivery fee */}
-          <div className="ord-det-tab-4">
-            <article className="ord-det-tab-del">Delivery Fee</article>
-            <article className="ord-det-tab-del-2">{this.state.deliveryFee}</article>
+          <div className={`${styles.ord_det_tab_4}`}>
+            <article className={`${styles.ord_det_tab_del}`}>Delivery Fee</article>
+            <article className={`${styles.ord_det_tab_del_2}`}>{this.state.deliveryFee}</article>
           </div>
           {/* total */}
-          <div className="ord-det-tab-5">
-            <article className="ord-det-tab-tot">Total</article>
-            <article className="ord-det-tab-tot-2">{this.state.total}</article>
+          <div className={`${styles.ord_det_tab_5}`}>
+            <article className={`${styles.ord_det_tab_tot}`}>Total</article>
+            <article className={`${styles.ord_det_tab_tot_2}`}>{this.state.total}</article>
           </div>
           {/* delivery address */}
-          <div className="ord-det-tab-6">
-            <article className="ord-det-tab-add">Delivery Address: </article>
-            <article className="ord-det-tab-add-2">{this.state.address}</article>
+          <div className={`${styles.ord_det_tab_6}`}>
+            <article className={`${styles.ord_det_tab_add}`}>Delivery Address: </article>
+            <article className={`${styles.ord_det_tab_add_2}`}>{this.state.address}</article>
           </div>
           {/* vendor */}
-          <div className="ord-det-tab-7">
-            <article className="ord-det-tab-ven">Vendor: </article>
-            <article className="ord-det-tab-ven-2">{this.state.vendor}</article>
+          <div className={`${styles.ord_det_tab_7}`}>
+            <article className={`${styles.ord_det_tab_ven}`}>Vendor: </article>
+            <article className={`${styles.ord_det_tab_ven_2}`}>{this.state.vendor}</article>
           </div>
           {/* order id */}
-          <div className="ord-det-tab-8">
-            <article className="ord-det-tab-id">Order ID: </article>
-            <article className="ord-det-tab-id-2">{this.state.orderID}</article>
+          <div className={`${styles.ord_det_tab_8}`}>
+            <article className={`${styles.ord_det_tab_id}`}>Order ID: </article>
+            <article className={`${styles.ord_det_tab_id_2}`}>{this.state.orderID}</article>
           </div>
         </div>
       </div>
